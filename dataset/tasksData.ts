e49aa1565c7b110ef498930915390c61f4856d2c
kicky1 <krzysztofwicki1@gmail.com>
2023-09-29 12:37:40 +0200

    feat: add first task

--- dataset/tasksData.ts
+++ dataset/tasksData.ts
@@ -2,12 +2,14 @@ export const tasksData = [
     {
       id: 1,
       title: "T.1",
+      link: "points",
       description:
         "Create app whitch will save points on mouse click, and then store it to mange undo created point.",
     },
     {
       id: 2,
       title: "T.2",
+      link: "points",
       description:
         "Create app whitch will save points on mouse click, and then store it to mange undo created point.",
     },
