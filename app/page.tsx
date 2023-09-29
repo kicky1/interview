a7204c0c0787978498cc3ccc092c8f3cc7b7127a
kicky1 <krzysiekwicki1@gmail.com>
2023-09-29 14:21:07 +0200

    feat: change styles for cards

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home () {
           Interview <code className="font-mono font-bold">Tasks</code>
         </p>
       </div>
-      <div className="max-w-7xl mx-auto p-4 grid grid-cols-2 md:grid-cols-4 gap-12">
+      <div className="max-w-7xl mx-auto p-4 grid grid-cols-1 md:grid-cols-3 gap-12">
         <Tasks />
       </div>
     </div>
