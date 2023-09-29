9bcf459584dfbf4ec729c494d5f12bfbc99d5fdd
kicky1 <krzysiekwicki1@gmail.com>
2023-09-29 13:19:11 +0200

    feat: clear project

--- app/task/points/page.tsx
+++ app/task/points/page.tsx
@@ -1,13 +1,24 @@
-'use client'
+"use client";
 
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
 
 export default function Page() {
-  const [points, setPoints] = useState<{ x: number; y: number; visible: boolean}[]>([]);
+  const [points, setPoints] = useState<
+    { x: number; y: number; visible: boolean }[]
+  >([]);
 
   const handleClick: React.MouseEventHandler<HTMLDivElement> = (e) => {
-    setPoints([...points, { x: e.clientX, y: e.clientY, visible: true }]);
+    const container = e.currentTarget;
+    const x =
+      ((e.clientX - container.getBoundingClientRect().left) /
+        container.clientWidth) *
+      100;
+    const y =
+      ((e.clientY - container.getBoundingClientRect().top) /
+        container.clientHeight) *
+      100;
+    setPoints([...points, { x, y, visible: true }]);
   };
 
   const handleClearPoints = () => {
@@ -25,7 +36,6 @@ export default function Page() {
     }
   };
 
-
   const handleRestore = () => {
     const updatedPoints = [...points];
     for (let i = updatedPoints.length - 1; i >= 0; i--) {
@@ -38,12 +48,12 @@ export default function Page() {
   };
 
   const checkIfAllPointsAreVisible = () => {
-   return points.every((point) => point.visible);
-  }
+    return points.every((point) => point.visible);
+  };
 
   const checkIfAllPointsAreInvisible = () => {
     return points.every((point) => !point.visible);
-  }
+  };
 
   return (
     <>
@@ -51,22 +61,42 @@ export default function Page() {
         <p className="text-2xl">
           Task 1: <code className="font-mono font-bold">"Map of points"</code>
         </p>
-        <div 
-          className="box-content h-96 w-96 p-4 my-16 border-2 border-teal-600 rounded" 
+        <div
+          className="box-content relative h-96 w-96 p-4 my-16 border-2 border-teal-600 rounded"
           onClick={handleClick}
         >
-          {points.map((point, index) => (
-            point.visible && (
-            <div 
-              key={index} 
-              className="absolute w-4 h-4 bg-teal-600 rounded-full" 
-              style={{ top: point.y-7, left: point.x-7 }}
-            />
-          )))}
+          {points.map(
+            (point, index) =>
+              point.visible && (
+                <div
+                  key={index}
+                  className="absolute w-4 h-4 bg-teal-600 rounded-full"
+                  style={{ top: `${point.y - 2}%`, left: `${point.x - 2}%` }}
+                />
+              )
+          )}
         </div>
-          <Button className="m-2" onClick={handleClearPoints} disabled={points.length < 1}>Clear points</Button>
-          <Button className="m-2" onClick={handleUndo} disabled={checkIfAllPointsAreInvisible()}>Undo</Button>
-          <Button className="m-2" onClick={handleRestore} disabled={checkIfAllPointsAreVisible()}>Restore</Button>
+        <Button
+          className="m-2"
+          onClick={handleClearPoints}
+          disabled={points.length < 1}
+        >
+          Clear points
+        </Button>
+        <Button
+          className="m-2"
+          onClick={handleUndo}
+          disabled={checkIfAllPointsAreInvisible()}
+        >
+          Undo
+        </Button>
+        <Button
+          className="m-2"
+          onClick={handleRestore}
+          disabled={checkIfAllPointsAreVisible()}
+        >
+          Restore
+        </Button>
       </div>
     </>
   );
