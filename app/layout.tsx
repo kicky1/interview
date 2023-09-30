b2b1d15e8e341e5d77d12c42f9de281a4fa7fc04
kicky1 <krzysiekwicki1@gmail.com>
2023-09-30 22:06:32 +0200

    fix: type for alert variant

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,22 @@
-import './globals.css';
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Interview Tasks',
-  description: 'Web app for all interview tasks in React.js',
-};
+  description: 'Web app for all interview tasks in React.js'
+}
 
-export default function RootLayout({
-  children,
+export default function RootLayout ({
+  children
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-  );
+  )
 }
