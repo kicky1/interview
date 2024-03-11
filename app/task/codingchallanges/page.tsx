ff4b7e53c0f5d8f57b6b0ef157e051a66e8bfc56
Wicki Krzysztof <krzysiekwicki1@gmail.com>
2024-03-11 17:24:43 +0100

    feat: add accordion

--- app/task/codingchallanges/page.tsx
+++ app/task/codingchallanges/page.tsx
@@ -1,5 +1,11 @@
 import Challenge1 from "@/components/Challenges/Challenge1";
 import Challenge2 from "@/components/Challenges/Challenge2";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
 
 export default function Page() {
   return (
@@ -8,10 +14,21 @@ export default function Page() {
         <p className="text-2xl">
           Task 7: <code className="font-mono font-bold">"Coding Challanges"</code>
         </p>
-        <div className="flex flex-col items-center justify-start mt-16 text-left">
-          <Challenge1/>
-          <div className="mt-4"/>
-          <Challenge2/>
+        <div className="flex flex-col items-left justify-start mt-16 text-left">
+          <Accordion type="single" collapsible className="w-full">
+            <AccordionItem value="item-1">
+              <AccordionTrigger>Arrays</AccordionTrigger>
+              <AccordionContent>
+                <Challenge1/>
+              </AccordionContent>
+            </AccordionItem>
+            <AccordionItem value="item-2">
+              <AccordionTrigger>Strings</AccordionTrigger>
+              <AccordionContent>
+              <Challenge2/>
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
         </div>
       </div>
     </>
